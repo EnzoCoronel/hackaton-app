0025e3e09422e65d92b193c0da60ca5cdc5744d4
Enzo da Silva Coronel Garcia <enzocoronel1000@gmail.com>
2024-11-12 19:14:14 -0300

    feature: add shadcn component

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,21 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default function Home() {
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
@@ -36,6 +25,9 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
+
+        <Button>Button</Button>
+        <Button variant={"outline"}>Button</Button>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
